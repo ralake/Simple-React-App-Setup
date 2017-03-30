0fc7ad1e3dbb0aa724fbbad1b72f9af54d27260e
Richard Lake <ralake1985@gmail.com>
2017-03-30 16:29:47 +0100

    Initial setup

---
+++ src/client/components/test-component/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TestComponent = () => {
+  return <div className='Test'>TestComponent</div>
+}
+
+export default TestComponent
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const APP_DIR = `${__dirname}/src/client`
+const BUILD_DIR = `${__dirname}/public`
+
+module.exports = {
+  entry: `${APP_DIR}/app.js`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
